db0a5a599847dc16676a5769f42f27fb913784d2
HD-LB <66850616+HD-LB@users.noreply.github.com>
2021-11-17 10:15:59 -0700

    Update CartesianCoordinateSystem.cs

--- CSharp1TestPractice2/CSharpTestPractice2/CartesianCoordinateSystem.cs
+++ CSharp1TestPractice2/CSharpTestPractice2/CartesianCoordinateSystem.cs
@@ -5,7 +5,11 @@
 //http://telerikacademy.com/Courses/LectureResources/Video/5480/Exam-preparation-Coordinate-System-Tasks-%d0%95%d0%b2%d0%bb%d0%be%d0%b3%d0%b8
 //http://telerikacademy.com/Courses/LectureResources/Video/5454/%d0%9f%d1%80%d0%b8%d0%bc%d0%b5%d1%80%d0%b5%d0%bd-%d0%b8%d0%b7%d0%bf%d0%b8%d1%82-26-%d0%bd%d0%be%d0%b5%d0%bc%d0%b2%d1%80%d0%b8-2012-%d0%9d%d0%b8%d0%ba%d0%b8
 
-//You are given a two-dimensional Cartesian coordinate system and the two coordinates(X and Y) of a point in the coordinate system.If you don't know what Cartesian coordinate system is Google it with Bing. As you will find, the coordinate system is divided by 2 lines (see the picture bellow) which divide the plain in four parts. Each of these parts has a lot of points that are numbered between 1 and 4. There is one point where our lines are crossing. This point has the following coordinates: X=0 and Y=0. As a result this point is numbered 0. The points on the lines are also numbered with the numbers 5 and 6 (again see the picture below).
+//You are given a two-dimensional Cartesian coordinate system and the two coordinates(X and Y) of a point in the coordinate system.If you don't know what 
+Cartesian coordinate system is Google it with Bing. As you will find, the coordinate system is divided by 2 lines (see the picture bellow) which divide the 
+plain in four parts. Each of these parts has a lot of points that are numbered between 1 and 4. There is one point where our lines are crossing. This point 
+has the following coordinates: X=0 and Y=0. As a result this point is numbered 0. The points on the lines are also numbered with the numbers 5 and 6 (again 
+see the picture below).
 //Your task is to write a program that finds the number of the location of the given point in the coordinate system.
 
 //Input
